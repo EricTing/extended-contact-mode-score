6bd851f64b2ddd8e39c1bffb008d3710627fc4ac
EricTing <yunding.whu@gmail.com>
2015-09-28 11:10:30 -0500

    write the results to json file
    	modified:   src/mixed_resolution_xcms.py

--- src/mixed_resolution_xcms.py
+++ src/mixed_resolution_xcms.py
@@ -4,10 +4,13 @@
 import luigi
 import os
 import subprocess32
+import json
 from apoc_inputs import DecompressedPdb
 from run_control_apoc import LigandExpStructureInMol2
 from run_control_apoc import LigandMatchingList
 from run_control_apoc import ProteinMatchingList
+from run_control_apoc import LpcKcombuResult, LpcApocResultTask
+from run_control_apoc import PkcombuAtomMatchParser, ApocResultParer
 from urls import APOC_WORKING_DIR
 
 
@@ -36,12 +39,14 @@ class MixedResolutionXcms(luigi.Task):
                 LigandExpStructureInMol2(self.qname),
                 LigandMatchingList(self.tname, self.qname, self.subset),
                 ProteinMatchingList(self.tname, self.qname, self.subset),
+                LpcKcombuResult(self.tname, self.qname, self.subset),
+                LpcApocResultTask(self.tname, self.qname, self.subset),
                 DecompressedPdb(t_pdb),
                 DecompressedPdb(q_pdb)]
 
     def output(self):
         path = os.path.join(self.mydir(),
-                            "%s_%s.mxcms" % (self.tname, self.qname))
+                            "%s_%s_mixedres.json" % (self.tname, self.qname))
         return luigi.LocalTarget(path)
 
     def _run(self):
@@ -69,10 +74,51 @@ class MixedResolutionXcms(luigi.Task):
                 "--pml", pml_path]
 
         print " ".join(cmds)
-        subprocess32.call(cmds)
+        stdout = subprocess32.check_output(cmds)
+        data = {}
+        for line in stdout.splitlines():
+            if "#atom" in line:
+                data['# ligand atoms'] = int(line.split()[-1])
+            if "#res" in line:
+                data['# residues'] = int(line.split()[-1])
+            if "#xcms" in line:
+                data['xcms'] = float(line.split()[-1])
+        return data
+
+    def _kcombu_results(self):
+        path = LpcKcombuResult(self.tname,
+                               self.qname,
+                               self.subset).output().path
+        return PkcombuAtomMatchParser(path)
 
     def run(self):
-        self._run()
+        data = self._run()
+        data['Kcombu tanimoto'] = self._kcombu_results().data.tanimoto
+
+        with LpcApocResultTask(self.tname,
+                               self.qname,
+                               self.subset).output().open('r') as f:
+            apoc_parser = ApocResultParer(f.read())
+
+        data['tname'] = self.tname
+        data['qname'] = self.qname
+        data['Apoc result'] = LpcApocResultTask(self.tname, self.qname, self.subset).output().path
+        data['Kcombu result'] = LpcKcombuResult(self.tname, self.qname, self.subset).output().path
+
+        global_alignment = apoc_parser.queryGlobal(self.tname, self.qname)
+        data['seq identity'] = global_alignment.seq_identity
+        pocket_alignment = apoc_parser.queryPocket(self.tname, self.qname)
+        if pocket_alignment.has_pocket_alignment:
+            data['Apoc ps-score'] = pocket_alignment.ps_score
+            data['Apoc p-value'] = pocket_alignment.p_value
+            data['Apoc z-score'] = pocket_alignment.z_score
+
+        f = self.output().open('w')
+        to_write = json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+        f.write(to_write)
+        f.close()
+
+        print "xcms output %s" % (self.output().path)
 
 
 def main(tname, qname):
