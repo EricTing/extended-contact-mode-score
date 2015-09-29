1d04cca34a69ab9a1e6224382d5eec8a0c0ada98
EricTing <yunding.whu@gmail.com>
2015-09-29 15:32:30 -0500

    reduce the data for mixed resolution xCms
    	modified:   src/collect_xcms.py

--- src/collect_xcms.py
+++ src/collect_xcms.py
@@ -9,6 +9,7 @@ from urls import WORKING_DIR
 from xcms import LpcApocXcms
 from apoc_inputs import ApocListPathTask
 from run_control_apoc import LpcApocResultTask, ApocResultParer
+from mixed_resolution_xcms import MixedResolutionXcms
 
 
 class ApocPocketsResultsCollection(luigi.Task):
@@ -126,7 +127,7 @@ class AtomicXcmsTable(luigi.Task):
             dicts = pickle.load(f)
 
         cols = ['Apoc ps-score', 'Kcombu tanimoto',
-                'xcms', '# ligand atoms', '# residue atoms']
+                'xcms', '# ligand atoms', '# residues']
         datas = []
         # TODO: calculate the fraction of contact
         for key, data in dicts.iteritems():
@@ -148,14 +149,65 @@ class AtomicXcmsTable(luigi.Task):
             return pd.read_csv(inputObj, index_col=0)
 
 
+class MixedResolutionXcmsCollection(AtomicXcmsCollection):
+
+    def output(self):
+        collected = os.path.join(WORKING_DIR, self.subset + "_mixed_xcms.pkl")
+        missed = os.path.join(WORKING_DIR, self.subset + "_mixed_xcms.missed")
+        return luigi.LocalTarget(collected), luigi.LocalTarget(missed)
+
+    def run(self):
+        collected, missed = self.output()
+        collected_content = {}
+        missed_content = []
+        with self.requires().output().open('r') as f:
+            lines = f.read().splitlines()
+            count = 0
+            for line in lines:
+                count += 1
+                print("%s %f%%\n" % (line, count  * 100. / len(lines)))
+                try:
+                    tname, qname = line.split()
+                    key = tname + " " + qname
+                    lpc_result = MixedResolutionXcms(tname, qname, self.subset).output()
+                    if lpc_result.exists():
+                        with lpc_result.open('r') as f:
+                            collected_content[key] = json.loads(f.read())
+                    else:
+                        missed_content.append(key)
+                except ValueError:
+                    print "ERROR!", line
+                    pass
+
+        with collected.open('w') as f:
+            pickle.dump(collected_content, f)
+
+        with missed.open('w') as f:
+            for key in missed_content:
+                f.write(key + "\n")
+
+
+class MixedResolutionXcmsTable(AtomicXcmsTable):
+
+    def requires(self):
+        return MixedResolutionXcmsCollection(self.subset)
+
+    def output(self):
+        csv_path = os.path.join(WORKING_DIR, self.subset + "_mixed_xcms.csv")
+        return luigi.LocalTarget(csv_path)
+
+
+
 def main():
     luigi.build([AtomicXcmsCollection("subject"),
-                 AtomicXcmsCollection("rs2"),
-                 AtomicXcmsTable("subject"),
-                 ApocListPathTask("rs2"),
-                 ApocPocketsResultsCollection("rs2"),
-                 ApocPocketsResultsTable("rs2"),
-                 AtomicXcmsTable("rs2")],
+                 MixedResolutionXcmsTable("subject"),
+                 MixedResolutionXcmsTable("control"),
+                 # AtomicXcmsCollection("rs2"),
+                 AtomicXcmsTable("subject")],
+                 # ApocListPathTask("rs2"),
+                 # ApocPocketsResultsCollection("rs2"),
+                 # ApocPocketsResultsTable("rs2"),
+                 # AtomicXcmsTable("rs2")],
                 local_scheduler=True)
 
 
