3e16d4017200a7969b33eea43b59ca971f0c54e3
EricTing <yunding.whu@gmail.com>
2015-10-16 11:12:53 -0500

    re-cluster the confs using optmized paras
    	modified:   src/sample_confs.py

--- src/sample_confs.py
+++ src/sample_confs.py
@@ -20,10 +20,11 @@ class SampleConf(luigi.Task):
 
     def output(self):
         path = os.path.join(self.dirname(),
-                            self.sdf_id + ".csv")
+                            self.sdf_id,
+                            self.sdf_id + ".db_scan")
         return luigi.LocalTarget(path)
 
-    def run(self):
+    def get_data(self):
         ifn = os.path.join(self.dirname(),
                            self.sdf_id,
                            self.sdf_id + ".csv")
@@ -35,9 +36,11 @@ class SampleConf(luigi.Task):
         if len(conf_vecs) > total:
             conf_vecs = conf_vecs[:total]
 
-        ofn = os.path.join(self.dirname(),
-                           self.sdf_id,
-                           self.sdf_id + ".db_scan")
+        return conf_vecs
+
+    def run(self):
+        conf_vecs = self.get_data()
+        ofn = self.output().path
 
         with open(ofn, 'w') as ofs:
             ofs.write("min_samples eps #cluster ss\n")
@@ -54,8 +57,45 @@ class SampleConf(luigi.Task):
                         pass
 
 
+class ClusterConfs(SampleConf):
+
+    def requires(self):
+        return SampleConf(self.sdf_id)
+
+    def output(self):
+        path = os.path.join(self.dirname(),
+                            self.sdf_id,
+                            self.sdf_id + "_clustered.csv")
+        return luigi.LocalTarget(path)
+
+    def get_cluster_paras(self):
+        ifn = SampleConf(self.sdf_id).output().path
+        dset = pd.read_csv(ifn, sep=' ', index_col=False)
+        dset = dset[dset['ss'] > 0.0]
+        target_cluster_num = 100
+        dset['Dist2Hundred'] = dset['#cluster'].apply(lambda x: abs(x - target_cluster_num))
+        dset['deviedByss'] = dset['Dist2Hundred'] / dset['ss']
+        sorted_dset = dset.sort(['deviedByss'])
+        min_samples, eps = sorted_dset.values[0][:2]
+        return min_samples, eps
+
+    def run(self):
+        min_samples, eps = self.get_cluster_paras()
+        conf_vecs = self.get_data()
+        db = DBSCAN(eps=eps, min_samples=min_samples).fit(conf_vecs)
+
+        ifn = os.path.join(self.dirname(),
+                           self.sdf_id,
+                           self.sdf_id + ".csv")
+
+        dset = pd.read_csv(ifn, sep=' ', index_col=False)
+        dset.ix[db.core_sample_indices_].to_csv(self.output().path,
+                                                sep=' ',
+                                                index=False)
+
+
 def main(sdf):
-    luigi.build([SampleConf(sdf)],
+    luigi.build([SampleConf(sdf), ClusterConfs(sdf)],
                 local_scheduler=True)
 
 
