cf3207948ff8b20b71fa9e2d17255b8e82575d40
EricTing <yunding.whu@gmail.com>
2015-10-17 15:11:59 -0500

    use grid method to sample the variational conformations
    	modified:   readme.org
    	renamed:    src/apc_pbs/wq_astex_kmeans.pbs -> src/apc_pbs/wq_astex_grid.pbs
    	modified:   src/sample_confs.py

--- src/sample_confs.py
+++ src/sample_confs.py
@@ -2,9 +2,7 @@
 
 import os
 import pandas as pd
-from sklearn.cluster import KMeans
 import numpy as np
-from sklearn import metrics
 import luigi
 
 
@@ -21,7 +19,7 @@ class SampleConf(luigi.Task):
     def output(self):
         path = os.path.join(self.dirname(),
                             self.sdf_id,
-                            self.sdf_id + "_kmeans.csv")
+                            self.sdf_id + "_grid.csv")
         return luigi.LocalTarget(path)
 
     def run(self):
@@ -34,18 +32,38 @@ class SampleConf(luigi.Task):
         if len(dset > total_samples):
             rows = np.random.choice(dset.index.values, total_samples)
             dset = dset.ix[rows]
-        conf_vecs = dset[['t0', 't1', 't2', 'r0', 'r1', 'r2']].values.copy()
+
+        xg = np.linspace(dset.t0.min(), dset.t0.max(), 10)
+        yg = np.linspace(dset.t1.min(), dset.t1.max(), 10)
+        zg = np.linspace(dset.t2.min(), dset.t2.max(), 10)
+
+        dx = xg[1] - xg[0]
+        dy = yg[1] - yg[0]
+        dz = zg[1] - zg[0]
+
+        grid_pts_with_confs = pd.DataFrame()
+        for x in xg:
+            for y in yg:
+                for z in zg:
+                    mydset = dset[(dset.t0 > x)
+                                  & (dset.t0 < x + dx)
+                                  & (dset.t1 > y)
+                                  & (dset.t1 < y + dy)
+                                  & (dset.t2 > z)
+                                  & (dset.t2 < z + dz)]
+                    if len(mydset) > 0:
+                        mydset.index = range(len(mydset))
+                        grid_pts_with_confs = pd.concat([grid_pts_with_confs, mydset.ix[[0]]])
 
         num_clusters = 100
-        kmeans = KMeans(n_clusters=num_clusters, n_jobs=16)
-        kmeans.fit(conf_vecs)
-        centers = kmeans.cluster_centers_
+        if len(grid_pts_with_confs) > num_clusters:
+            rows = np.random.choice(grid_pts_with_confs.index.values, num_clusters)
+            grid_pts_with_confs = grid_pts_with_confs.ix[rows]
 
+        cols = dset.columns
+        grid_pts_with_confs = grid_pts_with_confs[cols]
         ofn = self.output().path
-        with open(ofn, 'w') as ofs:
-            ofs.write("lig prt t0 t1 t2 r0 r1 r2\n")
-            for center in centers:
-                ofs.write("0 0 " + " ".join(map(str, center)) + "\n")
+        grid_pts_with_confs.to_csv(ofn, sep=' ', index=False)
 
 
 def main(sdf):
