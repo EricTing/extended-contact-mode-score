c9ffc544ed6eae698b9b8aca771fd7bb34be406a
EricTing <yunding.whu@gmail.com>
2015-10-25 18:38:22 -0500

    calculate xcms for RS2
    	modified:   src/disasembling.py

--- src/disasembling.py
+++ src/disasembling.py
@@ -5,7 +5,7 @@ import shlex
 import os
 import subprocess32
 
-from run_control_apoc import PdbMol2
+from run_control_apoc import PdbMol2, PkcombuAtomMatchParser
 from urls import WORKING_DIR
 import glob
 
@@ -73,7 +73,7 @@ class DisasemblePdb(Path):
         return PdbMol2(self.pdb_id)
 
     def output(self):
-        pass
+        return luigi.LocalTarget(self.dirname())
 
     def run(self):
         cif_ifn = self.requires().output().path
@@ -83,8 +83,95 @@ class DisasemblePdb(Path):
         subprocess32.call(args)
 
 
-def main(pdb_id):
-    luigi.build([DisasemblePdb(pdb_id)],
+class Code:
+    def __init__(self, code):
+        self.code = code
+        tokens = self.code.split('_')
+        self.pdb_id = tokens[0]
+        self.lig_id = tokens[1]
+        self.chain_id = tokens[2]
+
+
+class ApocRandomSet2(luigi.Task):
+
+    tname = luigi.Parameter()
+    qname = luigi.Parameter()
+
+    def my_midtwo(self):
+        return self.tname[1:3]
+
+    def my_dir(self):
+        path = os.path.join(WORKING_DIR,
+                            self.my_midtwo(),
+                            self.tname)
+        try:
+            os.makedirs(path)
+        except:
+            pass
+        return path
+
+    def requires(self):
+        tcode = Code(self.tname)
+        qcode = Code(self.qname)
+        return [DisasemblePdb(tcode.pdb_id),
+                DisasemblePdb(qcode.pdb_id)]
+
+    def output(self):
+        path = os.path.join(self.my_dir(),
+                            self.tname + '__' + self.qname + ".xcms")
+        return luigi.LocalTarget(path)
+
+    def runKcombu(self, lig1_path, lig2_path):
+        oam_path = os.path.join(self.my_dir(),
+                                self.tname + '__' + self.qname + ".oam")
+        cmds = ["pkcombu",
+                "-A", lig1_path,
+                "-B", lig2_path,
+                "-oam", oam_path]
+        try:
+            print " ".join(cmds)
+            subprocess32.call(cmds)
+            return oam_path
+        except:
+            pass
+
+    def PrtAndLig(self, name):
+        code = Code(name)
+        pdb_id = code.pdb_id
+        src_dir = DisasemblePdb(pdb_id).output().path
+        mol_fn = "%s_%s%s_%s.mol2" % (pdb_id.upper(), pdb_id.upper(),
+                                      code.chain_id,
+                                      code.lig_id.upper())
+        pdb_fn = "%s_%s%s.pdb" % (pdb_id.upper(),
+                                  pdb_id.upper(), code.chain_id)
+        return os.path.join(src_dir, pdb_fn), os.path.join(src_dir, mol_fn)
+
+    def run(self):
+        pdb1, mol1 = self.PrtAndLig(self.tname)
+        pdb2, mol2 = self.PrtAndLig(self.qname)
+        paths = [pdb1, pdb2, mol1, mol2]
+        if not all(map(os.path.exists, paths)):
+            print "not enough inputs"
+        else:
+            oam_path = self.runKcombu(mol1, mol2)
+            parser = PkcombuAtomMatchParser(oam_path)
+            output_path = self.output().path
+            lml_path = os.path.splitext(output_path)[0] + '.lml'
+            parser.writeMatchingSerialNums(lml_path)
+            cmds = ['run_xcms',
+                    '--la', mol1,
+                    '--lb', mol2,
+                    '--pa', pdb1,
+                    '--pb', pdb2,
+                    '--lml', lml_path]
+            print " ".join(cmds)
+            stdout = subprocess32.check_output(cmds)
+            with open(output_path, 'w') as ofs:
+                ofs.write(stdout)
+
+
+def main(tname, qname):
+    luigi.build([ApocRandomSet2(tname, qname)],
                 local_scheduler=True)
 
 
@@ -98,7 +185,8 @@ def check(sdf_id):
 
 if __name__ == '__main__':
     import sys
-    pdb_id = sys.argv[1]
-    main(pdb_id)
+    tname = sys.argv[1]
+    qname = sys.argv[2]
+    main(tname, qname)
     # sdf_id = sys.argv[1]
     # check(sdf_id)
