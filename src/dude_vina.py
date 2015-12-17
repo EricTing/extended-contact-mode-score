75b95e55249d9be46ef08a7f1d3b4d1496916d2e
EricTing <yunding.whu@gmail.com>
2015-12-17 17:05:01 -0600

    calcualte sparmanR for vina predictions on dude
    	modified:   src/dude_vina.py

--- src/dude_vina.py
+++ src/dude_vina.py
@@ -12,6 +12,8 @@ from collections import defaultdict
 
 from run_control_apoc import PkcombuAtomMatchParser
 from astex_xcms import runPkcombu
+from single_complex_cms import readPrtCoords, contactVector
+from scipy.stats import spearmanr
 
 
 class CheckNames(luigi.Task):
@@ -112,36 +114,36 @@ class Calculate(luigi.Task):
                             self.subset, self.tar_name + '.json')
         return luigi.LocalTarget(path)
 
+    def untar(self):
+        my_dir = '/var/scratch/' + os.getenv('USER')
+        try:
+            os.makedirs(my_dir)
+        except:
+            pass
+
+        targz_path = os.path.join("/work/jaydy/dat/edud_vina",
+                                    self.subset, self.tar_name + '.tar.gz')
+        cmds = [
+            'tar', 'xf', targz_path,
+            '-C', my_dir
+        ]
+        subprocess32.call(cmds)
+        return os.path.join(my_dir, self.tar_name)
+
+    def getPdbqtFiles(self, root_dir):
+        paths = []
+        for (dirname, subshere, fileshere) in os.walk(root_dir):
+            for fname in fileshere:
+                if fname.endswith('pdbqt'):
+                    path = os.path.join(dirname, fname)
+                    paths.append(path)
+        return paths
+
     def run(self):
-        def untar():
-            my_dir = '/var/scratch/' + os.getenv('USER')
-            try:
-                os.makedirs(my_dir)
-            except:
-                pass
-
-            targz_path = os.path.join("/work/jaydy/dat/edud_vina",
-                                      self.subset, self.tar_name + '.tar.gz')
-            cmds = [
-                'tar', 'xf', targz_path,
-                '-C', my_dir
-            ]
-            subprocess32.call(cmds)
-            return os.path.join(my_dir, self.tar_name)
-
-        def getPdbqtFiles(root_dir):
-            paths = []
-            for (dirname, subshere, fileshere) in os.walk(root_dir):
-                for fname in fileshere:
-                    if fname.endswith('pdbqt'):
-                        path = os.path.join(dirname, fname)
-                        paths.append(path)
-            return paths
-
         results = defaultdict(dict)
 
-        untared_dir = untar()
-        pdbqt_fns = getPdbqtFiles(untared_dir)
+        untared_dir = self.untar()
+        pdbqt_fns = self.getPdbqtFiles(untared_dir)
 
         def runXcms():
             for fn in pdbqt_fns:
@@ -218,9 +220,112 @@ class Calculate(luigi.Task):
         shutil.rmtree(untared_dir)
 
 
+class SpearmanR(Calculate):
+    def output(self):
+        try:
+            os.makedirs(os.path.join("/work/jaydy/working", self.subset))
+        except:
+            pass
+        path = os.path.join("/work/jaydy/working",
+                            self.subset, self.tar_name + '_spearmanr.json')
+        return luigi.LocalTarget(path)
+
+    def run(self):
+
+        untared_dir = self.untar()
+        pdbqt_fns = self.getPdbqtFiles(untared_dir)
+
+        def readLigandCoords(path, mlist, format="sdf"):
+            mol = pybel.readfile(format, path).next()
+            mol.removeh()
+            all_coords = [atom.coords for atom in mol
+                          if not atom.OBAtom.IsHydrogen()]
+            coords = []
+            for idx in mlist:
+                coords.append(all_coords[idx - 1])
+
+            return coords
+
+        def runSpearmanR():
+            results = defaultdict(dict)
+            for fn in pdbqt_fns:
+                try:
+                    lig_sdf = os.path.splitext(fn)[0] + '.sdf'
+
+                    if not os.path.exists(lig_sdf):
+                        cmds = ['obabel', '-ipdbqt', fn,
+                                '-osdf', '-O', lig_sdf]
+                        subprocess32.call(cmds)
+                        # only the first conformer is needed
+                        lig = pybel.readfile("sdf", lig_sdf).next()
+                        lig.removeh()
+                        lig.write(format="sdf",
+                                  filename=lig_sdf, overwrite=True)
+
+                    prt_id = os.path.split(fn)[-1].split('-')[0]
+                    crystal_lig_sdf = LigSdf(prt_id).getSdfPath()
+
+                    oam_path = os.path.splitext(fn)[0] + '.oam'
+
+                    runPkcombu(lig_sdf, crystal_lig_sdf, oam_path)
+                    kcombu = PkcombuAtomMatchParser(oam_path)
+                    list_a, list_b = kcombu.getMatchingSerialNums()
+                    predicted_lig_coords = readLigandCoords(lig_sdf, list_a)
+                    crystal_lig_coords = readLigandCoords(
+                        crystal_lig_sdf, list_b)
+
+                    crystal_prt_pdb = PrtPdb(prt_id).getPdbPath()
+                    prt_coords = readPrtCoords(crystal_prt_pdb)
+
+                    crystal_vec = contactVector(crystal_lig_coords,
+                                               prt_coords)
+                    predicted_vec = contactVector(predicted_lig_coords,
+                                                  prt_coords)
+
+                    max_pmf = 7.38 + 0.1
+                    neighboring_vec, neighboring_crystal_vec = [], []
+                    for dist, crystal_dist in zip(predicted_vec, crystal_vec):
+                        if dist < max_pmf or crystal_dist < max_pmf:
+                            neighboring_vec.append(dist)
+                            neighboring_crystal_vec.append(crystal_dist)
+
+                    contact_pairs = len(neighboring_crystal_vec)
+                    lig_id = os.path.basename(lig_sdf)
+                    tc = kcombu.getTc()
+                    lig_size = len(list_a)
+                    spearman = spearmanr(neighboring_vec,
+                                         neighboring_crystal_vec)
+
+                    results[prt_id][lig_id] = {
+                        "Tc": tc,
+                        'dude_ligand_indices': list_a,
+                        'crystal_ligand_indices': list_b,
+                        'contact_pairs': contact_pairs,
+                        'lig_size': lig_size,
+                        'prt_pt_num': contact_pairs / float(lig_size),
+                        'spearmanr': spearman.correlation,
+                        'pval': spearman.pvalue
+                    }
+                except:
+                    print("%s FAILS" % (fn), file=sys.stderr)
+                    pass
+
+                return results
+
+        results = runSpearmanR()
+        to_write = json.dumps(results, sort_keys=True,
+                              indent=4, separators=(',', ': '))
+        ofn = self.output().path
+        print(ofn)
+        with open(ofn, 'w') as ofs:
+            ofs.write(to_write)
+
+
 def main(task_name):
     luigi.build([
         Calculate(task_name,
+                  'output-crystal-active-mod-optimal'),
+        SpearmanR(task_name,
                   'output-crystal-active-mod-optimal'),
     ],
                 local_scheduler=True
