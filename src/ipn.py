c16fd6e584d87a3e8888435c7de7828d163f34bf
EricTing <yunding.whu@gmail.com>
2016-03-21 13:22:13 -0500

    update script for paper figures
    	modified:   src/ipn.py

--- src/ipn.py
+++ src/ipn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """paper figures
 """
+from __future__ import print_function
 
 import re
 import pandas as pd
@@ -51,7 +52,7 @@ def main():
     plt.xlabel("Pocket similarity")
     plt.ylabel("Ligand similarity")
     plt.tight_layout()
-    plt.savefig("/work/jaydy/working/p_val.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/p_val.tiff", dpi=200)
 
     plt.figure()
     df = native_df
@@ -67,7 +68,7 @@ def main():
     plt.xlabel("Pocket similarity")
     plt.ylabel("Ligand similarity")
     plt.tight_layout()
-    plt.savefig("/work/jaydy/working/native_xcms.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/native_xcms.tiff", dpi=200)
 
     plt.figure()
     df = fixed_df
@@ -83,7 +84,7 @@ def main():
     plt.xlabel("Pocket similarity")
     plt.ylabel("Ligand similarity")
     plt.tight_layout()
-    plt.savefig("/work/jaydy/working/predicted_xcms.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/predicted_xcms.tiff", dpi=200)
 
     plt.figure()
     df = rnd_df
@@ -99,7 +100,7 @@ def main():
     plt.xlabel("Pocket similarity")
     plt.ylabel("Ligand similarity")
     plt.tight_layout()
-    plt.savefig("/work/jaydy/working/random_xcms.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/random_xcms.tiff", dpi=200)
 
     predicted_rmsd = pd.read_csv(
         readme.CheckVinaResultAccuracy().output().path,
@@ -107,28 +108,29 @@ def main():
 
     plt.figure()
     predicted_rmsd.rmsd.hist(bins=30, color='darkgrey', alpha=0.9)
-    plt.xlabel("RMSD")
-    plt.savefig("/work/jaydy/working/predict_rmsd_hist.tiff", dpi=50)
+    plt.xlabel("RMSD [$\AA$]")
+    plt.savefig("/work/jaydy/working/xcms_plot/predict_rmsd_hist.tiff",
+                dpi=200)
 
     plt.figure()
     predicted_rmsd.cms.hist(bins=30, color='darkgrey', alpha=0.9)
     plt.xlabel("CMS")
     plt.xlim((0, 1))
-    plt.savefig("/work/jaydy/working/predict_cms_hist.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/predict_cms_hist.tiff", dpi=200)
 
     rnd_rmsd = pd.read_csv(readme.CheckVinaRandomRmsd().output().path,
                            index_col=0)
 
     plt.figure()
     rnd_rmsd.rmsd.hist(bins=30, color='darkgrey', alpha=0.9)
-    plt.xlabel("RMSD")
-    plt.savefig("/work/jaydy/working/random_rmsd_hist.tiff", dpi=50)
+    plt.xlabel("RMSD [$\AA$]")
+    plt.savefig("/work/jaydy/working/xcms_plot/random_rmsd_hist.tiff", dpi=200)
 
     plt.figure()
     rnd_rmsd.cms.hist(bins=30, color='darkgrey', alpha=0.9)
     plt.xlabel("CMS")
     plt.xlim((0, 1))
-    plt.savefig("/work/jaydy/working/random_cms_hist.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/random_cms_hist.tiff", dpi=200)
 
     native_df = readme.similarPocketsLigands(native_df)
     fixed_df = readme.similarPocketsLigands(fixed_df)
@@ -150,7 +152,8 @@ def main():
     plt.figure()
     fixed_spearmanr.spearmanr.hist(bins=30, color='darkgrey', alpha=0.9)
     plt.xlabel("XCMS")
-    plt.savefig("/work/jaydy/working/predict_xcms_hist.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/predict_xcms_hist.tiff",
+                dpi=200)
 
     rnd_xcms = rnd_df[["query", "spearmanr", "pval", "tc_times_ps", "TM-score",
                        "template"]]
@@ -163,70 +166,108 @@ def main():
     plt.figure()
     rnd_spearmanr.spearmanr.hist(bins=30, color='darkgrey', alpha=0.9)
     plt.xlabel("XCMS")
-    plt.savefig("/work/jaydy/working/random_xcms_hist.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/random_xcms_hist.tiff", dpi=200)
 
     plt.figure()
     fixed_spearmanr.plot(kind="scatter",
                          x='cms',
                          y='rmsd',
                          color='k',
-                         alpha=0.5)
+                         alpha=0.3)
+
+    # z = np.polyfit(fixed_spearmanr.cms, fixed_spearmanr.rmsd, 3)
+    # p = np.poly1d(z)
+    # x = np.linspace(0, 1, num=50, endpoint=True)
+    # fitted_line = p(x)
+    # plt.plot(x, fitted_line, 'r--')
+
+    # z = np.polyfit(fixed_spearmanr.cms, fixed_spearmanr.rmsd, 2)
+    # p = np.poly1d(z)
+    # x = np.linspace(0, 1, num=50, endpoint=True)
+    # fitted_line = p(x)
+    # plt.plot(x, fitted_line, 'b--')
+
+    # z = np.polyfit(fixed_spearmanr.cms, fixed_spearmanr.rmsd, 1)
+    # p = np.poly1d(z)
+    # x = np.linspace(0, 1, num=50, endpoint=True)
+    # fitted_line = p(x)
+    # plt.plot(x, fitted_line, 'r--')
+    # fixed_spearmanr[['cms', 'rmsd']].to_csv("/work/jaydy/working/cms_rmsd_scatter.csv")
+
     plt.xlabel("CMS")
-    plt.ylabel("RMSD")
+    plt.ylabel("RMSD [$\AA$]")
     plt.xlim((0, 1))
-    plt.savefig("/work/jaydy/working/cms_rmsd_scatter.tiff", dpi=50)
+    plt.ylim((0, 15))
+    plt.savefig("/work/jaydy/working/xcms_plot/cms_rmsd_scatter.tiff", dpi=200)
 
     high_tm = fixed_spearmanr[fixed_spearmanr["TM-score"] > 0.5]
     low_tm = fixed_spearmanr[fixed_spearmanr["TM-score"] < 0.5]
 
-    high_tm.to_csv("/work/jaydy/working/high_tm.csv", ignore_index=True)
-    low_tm.to_csv("/work/jaydy/working/low_tm.csv", ignore_index=True)
+    high_tm.to_csv("/work/jaydy/working/xcms_plot/high_tm.csv",
+                   ignore_index=True)
+    low_tm.to_csv("/work/jaydy/working/xcms_plot/low_tm.csv",
+                  ignore_index=True)
 
     plt.figure()
-    high_tm.plot(kind="scatter", x="cms", y="spearmanr", color='k', alpha=0.5)
+    # high_tm.plot(kind="scatter", x="cms", y="spearmanr", color='g', alpha=0.5, label='High global similarity')
+    plt.scatter(high_tm.cms,
+                high_tm.spearmanr,
+                color='g',
+                alpha=0.5,
+                label='High global similarity')
+    # plt.xlabel("CMS")
+    # plt.ylabel("XCMS")
+    # plt.savefig("/work/jaydy/working/xcms_plot/cms_xcms_high_scatter.tiff", dpi=200)
+
+    # plt.figure()
+    # low_tm.plot(kind="scatter", x="cms", y="spearmanr", color='r', alpha=0.5, label='Low global similarity')
+    plt.scatter(low_tm.cms,
+                low_tm.spearmanr,
+                color='r',
+                alpha=0.5,
+                label='Low global similarity')
+
     plt.xlim((0, 1))
     plt.ylim((-0.6, 1))
+    # plt.xlim((0, 1))
+    # plt.ylim((-0.6, 1))
+    # plt.legend(loc='upper left')
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102),
+               loc=3,
+               ncol=2,
+               mode="expand",
+               borderaxespad=0.)
     plt.xlabel("CMS")
     plt.ylabel("XCMS")
-    plt.savefig("/work/jaydy/working/cms_xcms_high_scatter.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/cms_xcms_high_low_scatter.tiff",
+                dpi=200)
 
-    plt.figure()
-    low_tm.plot(kind="scatter", x="cms", y="spearmanr", color='k', alpha=0.5)
-    plt.xlim((0, 1))
-    plt.ylim((-0.6, 1))
-    plt.xlabel("CMS")
-    plt.ylabel("XCMS")
-    plt.savefig("/work/jaydy/working/cms_xcms_low_scatter.tiff", dpi=50)
+    # plt.figure()
+    # high_tm.plot(kind="scatter", x="rmsd", y="spearmanr", color='k', alpha=0.5)
+    # plt.xlim((0, 18))
+    # plt.ylim((-0.8, 1))
+    # plt.xlabel("RMSD [$\AA$]")
+    # plt.ylabel("XCMS")
+    # plt.savefig("/work/jaydy/working/xcms_plot/rmsd_xcms_high_scatter.tiff", dpi=200)
 
-    plt.figure()
-    high_tm.plot(kind="scatter", x="rmsd", y="spearmanr", color='k', alpha=0.5)
-    plt.xlim((0, 18))
-    plt.ylim((-0.8, 1))
-    plt.xlabel("RMSD")
-    plt.ylabel("XCMS")
-    plt.savefig("/work/jaydy/working/rmsd_xcms_high_scatter.tiff", dpi=50)
-
-    plt.figure()
-    low_tm.plot(kind="scatter", x="rmsd", y="spearmanr", color='k', alpha=0.5)
-    plt.xlim((0, 18))
-    plt.ylim((-0.8, 1))
-    plt.xlabel("RMSD")
-    plt.ylabel("XCMS")
-    plt.savefig("/work/jaydy/working/rmsd_xcms_low_scatter.tiff", dpi=50)
-
-    low_tm[(low_tm['TM-score'] > 0.2) & (low_tm['rmsd'] < 2) & (low_tm[
-        'spearmanr'] > 0.6)]
+    # plt.figure()
+    # low_tm.plot(kind="scatter", x="rmsd", y="spearmanr", color='k', alpha=0.5)
+    # plt.xlim((0, 18))
+    # plt.ylim((-0.8, 1))
+    # plt.xlabel("RMSD [$\AA$]")
+    # plt.ylabel("XCMS")
+    # plt.savefig("/work/jaydy/working/xcms_plot/rmsd_xcms_low_scatter.tiff", dpi=200)
 
     plt.figure()
     from sklearn import metrics
     high_tm['native_like'] = high_tm.rmsd.apply(lambda r: 1 if r < 3 else 0)
     low_tm['native_like'] = low_tm.rmsd.apply(lambda r: 1 if r < 3 else 0)
-    hfpr, htpr, hthresholds = metrics.roc_curve(high_tm.native_like,
-                                                high_tm.spearmanr,
-                                                pos_label=1)
-    lfpr, ltpr, lthresholds = metrics.roc_curve(low_tm.native_like,
-                                                low_tm.spearmanr,
-                                                pos_label=1)
+    hfpr, htpr, _ = metrics.roc_curve(high_tm.native_like,
+                                      high_tm.spearmanr,
+                                      pos_label=1)
+    lfpr, ltpr, _ = metrics.roc_curve(low_tm.native_like,
+                                      low_tm.spearmanr,
+                                      pos_label=1)
 
     plt.plot(hfpr, htpr, 'k-', label="high global similarity")
     plt.plot(lfpr, ltpr, 'k--', label="low global similarity")
@@ -235,7 +276,7 @@ def main():
 
     print("High global similarity AUC: %f" % metrics.auc(hfpr, htpr))
     print("Low global similarity AUC: %f" % metrics.auc(lfpr, ltpr))
-    plt.savefig("/work/jaydy/working/xcms_roc.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/xcms_roc.tiff", dpi=200)
 
     plt.figure()
     rnd_spearmanr.plot(kind="scatter",
@@ -245,7 +286,7 @@ def main():
                        alpha=0.5)
     plt.xlim((0, 30))
     plt.ylim((-1, 1))
-    plt.xlabel("RMSD")
+    plt.xlabel("RMSD [$\AA$]")
     plt.ylabel("XCMS")
 
     xcms_back_df = pd.read_csv(
@@ -254,22 +295,80 @@ def main():
 
     xcms_back_df['query'] = xcms_back_df.index.values
 
-    predicted_rmsd.head()
-
     predicted_df = pd.merge(predicted_rmsd,
                             xcms_back_df[['query', 'spearmanr']])
 
     plt.figure()
-    predicted_df.plot(kind='scatter',
-                      x='cms',
-                      y='spearmanr',
-                      color='k',
-                      alpha=0.5)
+
+    sampled_predicted_df = predicted_df.sample(len(fixed_spearmanr))
+    sampled_predicted_df.plot(kind='scatter',
+                              x='cms',
+                              y='spearmanr',
+                              color='k',
+                              alpha=0.3)
+
+    # cleaned_predicted_df = predicted_df.dropna()
+
+    # z = np.polyfit(cleaned_predicted_df.cms, cleaned_predicted_df.spearmanr, 3)
+    # p = np.poly1d(z)
+    # x = np.linspace(0, 1, num=50, endpoint=True)
+    # fitted_line = p(x)
+    # plt.plot(x, fitted_line, 'r--')
+
+    # z = np.polyfit(cleaned_predicted_df.cms, cleaned_predicted_df.spearmanr, 2)
+    # p = np.poly1d(z)
+    # x = np.linspace(0, 1, num=50, endpoint=True)
+    # fitted_line = p(x)
+    # plt.plot(x, fitted_line, 'b--')
+
+    # z = np.polyfit(cleaned_predicted_df.cms, cleaned_predicted_df.spearmanr, 1)
+    # p = np.poly1d(z)
+    # x = np.linspace(0, 1, num=50, endpoint=True)
+    # fitted_line = p(x)
+    # plt.plot(x, fitted_line, 'r--')
+
+    # cleaned_predicted_df[['cms', 'spearmanr']].to_csv("/work/jaydy/working/cms_xcms_scatter.csv")
+
     plt.xlabel('CMS')
     plt.ylabel("XCMS")
     plt.xlim((0, 1))
     plt.ylim((-1, 1))
-    plt.savefig("/work/jaydy/working/cms_xcms_scatter.tiff", dpi=50)
+    plt.savefig("/work/jaydy/working/xcms_plot/cms_xcms_scatter.tiff", dpi=200)
+
+    plt.figure()
+    plt.hist([rnd_spearmanr.spearmanr, fixed_spearmanr.spearmanr],
+             30,
+             color=['lightgrey', 'dimgrey'],
+             label=['Random', 'AutoDock Vina Prediction'])
+    plt.xlabel('XCMS')
+    plt.ylabel('Number of complexes')
+    # plt.xlim((-1, 1))
+    plt.legend(loc='best')
+    plt.savefig("/work/jaydy/working/xcms_plot/rnd_pred_hist_xcms.tiff",
+                dpi=200)
+
+    plt.figure()
+    plt.hist([rnd_rmsd.rmsd, predicted_rmsd.rmsd],
+             30,
+             color=['lightgrey', 'dimgrey'],
+             label=['Random', 'AutoDock Vina Prediction'])
+    plt.xlabel('RMSD [$\AA$]')
+    plt.ylabel('Number of complexes')
+    plt.legend(loc='best')
+    plt.savefig("/work/jaydy/working/xcms_plot/rnd_pred_hist_rmsd.tiff",
+                dpi=200)
+
+    plt.figure()
+    plt.hist([rnd_rmsd.cms, predicted_rmsd.cms],
+             30,
+             color=['lightgrey', 'dimgrey'],
+             label=['Random', 'AutoDock Vina Prediction'])
+    plt.xlabel('CMS')
+    plt.ylabel('Number of complexes')
+    plt.xlim((0, 1))
+    plt.legend(loc='best')
+    plt.savefig("/work/jaydy/working/xcms_plot/rnd_pred_hist_cms.tiff",
+                dpi=200)
 
 
 if __name__ == '__main__':
